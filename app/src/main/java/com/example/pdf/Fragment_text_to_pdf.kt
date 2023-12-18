1465401fb3340435d89b39da06310e1ff9a0c7c9
Myetekk <mirekmich19@gmail.com>
2023-12-18 19:06:19 +0100

    wielostronicowość

--- app/src/main/java/com/example/pdf/Fragment_text_to_pdf.kt
+++ app/src/main/java/com/example/pdf/Fragment_text_to_pdf.kt
@@ -11,7 +11,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
-
+import android.widget.Toast
 
 
 class Fragment_text_to_pdf : Fragment() {
@@ -32,23 +32,26 @@ class Fragment_text_to_pdf : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        val content1 = "W tym tygodniu zatwierdzono slawetny aneks nr. 4. Co to oznacza? To ze w tym momencie mamy klepniete uzbrojenie i wyposazenie dla WSZYSTKICH TRZECH fregat rakietowych budowanych w ramach programu Miecznik. Okej, to nie oznacza 100% wyposazenia i uzbrojenia ale cyferke stojaca bardzo blisko tych 100% ktora uzasadnia stwierdzenie ze dla wszystkich 3 przyszlych fregat mamy uzbrojenie i wyposazenie. Koniec obaw o tym ze Miecznik skonczy jak Slazak 2.0 z gotowymi platformami ale bez pelnego uzbrojenia i wyposazenia, koniec obaw o to ze zamawiając reszte wyposazenia i uzbrojenia okaze sie ze albo jest ono znacznie drozsze albo 'mamy tylko nowy model'. Przed programem stoja kolejne kroki milowe i kolejne wyzwania, zas przed nami dziennikarzami obowiazek dalszego informowania i przekonywania, tak spoleczenstwa jak i nowej ekipy MON, o tym jak potrzebne MW RP i Polsce sa Mieczniki.   "
-//        val content2 = "W tym tygodniu zatwierdzono slawetny aneks nr. 4. Co to oznacza? To ze w tym momencie mamy klepniete uzbrojenie i wyposazenie dla WSZYSTKICH TRZECH fregat rakietowych budowanych w ramach programu Miecznik. Okej, to nie oznacza 100% wyposazenia i uzbrojenia ale cyferke stojaca bardzo blisko tych 100% ktora uzasadnia stwierdzenie ze dla wszystkich 3 przyszlych fregat mamy uzbrojenie i wyposazenie. Koniec obaw o tym ze Miecznik skonczy jak Slazak 2.0 z gotowymi platformami ale bez pelnego uzbrojenia i wyposazenia, koniec obaw o to ze zamawiając reszte wyposazenia i uzbrojenia okaze sie ze albo jest ono znacznie drozsze albo 'mamy tylko nowy model'. Przed programem stoja kolejne kroki milowe i kolejne wyzwania, zas przed nami dziennikarzami obowiazek dalszego informowania i przekonywania, tak spoleczenstwa jak i nowej ekipy MON, o tym jak potrzebne MW RP i Polsce sa Mieczniki.   "
-//        val content3 = "W tym tygodniu zatwierdzono slawetny aneks nr. 4. Co to oznacza? To ze w tym momencie mamy klepniete uzbrojenie i wyposazenie dla WSZYSTKICH TRZECH fregat rakietowych budowanych w ramach programu Miecznik. Okej, to nie oznacza 100% wyposazenia i uzbrojenia ale cyferke stojaca bardzo blisko tych 100% ktora uzasadnia stwierdzenie ze dla wszystkich 3 przyszlych fregat mamy uzbrojenie i wyposazenie. Koniec obaw o tym ze Miecznik skonczy jak Slazak 2.0 z gotowymi platformami ale bez pelnego uzbrojenia i wyposazenia, koniec obaw o to ze zamawiając reszte wyposazenia i uzbrojenia okaze sie ze albo jest ono znacznie drozsze albo 'mamy tylko nowy model'. Przed programem stoja kolejne kroki milowe i kolejne wyzwania, zas przed nami dziennikarzami obowiazek dalszego informowania i przekonywania, tak spoleczenstwa jak i nowej ekipy MON, o tym jak potrzebne MW RP i Polsce sa Mieczniki.   "
-//        val content = content1 + content2 + content3
-//        PdfGenerator.generatePdf(content, "example.pdf")
-
-
-
         val save = view.findViewById<ImageButton>(R.id.save_text_to_pdf)
         val clear = view.findViewById<ImageButton>(R.id.clear_text_to_pdf)
 
-
-
         save.setOnClickListener{
+
             textView_text_to_pdf = view.findViewById<TextView>(R.id.textView_text_to_pdf)
-            val content = textView_text_to_pdf.text.toString()
-            PdfGenerator.generatePdf(content, "example.pdf")
+//            val content = textView_text_to_pdf.text.toString()
+            val content = "W tym tygodniu zatwierdzono slawetny aneks nr. 4. Co to oznacza? To ze w tym momencie mamy klepniete uzbrojenie i wyposazenie dla WSZYSTKICH TRZECH fregat rakietowych budowanych w ramach programu Miecznik. Okej, to nie oznacza 100% wyposazenia i uzbrojenia ale cyferke stojaca bardzo blisko tych 100% ktora uzasadnia stwierdzenie ze dla wszystkich 3 przyszlych fregat mamy uzbrojenie i wyposazenie. Koniec obaw o tym ze Miecznik skonczy jak Slazak 2.0 z gotowymi platformami ale bez pelnego uzbrojenia i wyposazenia, koniec obaw o to ze zamawiajac reszte wyposazenia i uzbrojenia okaze sie ze albo jest ono znacznie drozsze albo 'mamy tylko nowy model'. Przed programem stoja kolejne kroki milowe i kolejne wyzwania, zas przed nami dziennikarzami obowiazek dalszego informowania i przekonywania, tak spoleczenstwa jak i nowej ekipy MON, o tym jak potrzebne MW RP i Polsce sa Mieczniki.   "
+
+
+            val newFile1 = PdfGenerator()
+            newFile1.fileName = "PDFiara_text.pdf"
+            newFile1.addText(content)
+            newFile1.addText("Taka sobie druga linia ")
+            newFile1.addText("Jakas trzecia linia ")
+            newFile1.addText("A tu czwarta ")
+//            newFile1.addText("ĄąĆćĘęŁłŃńÓóŚśŻżŹź")
+            newFile1.saveText()
+            newFile1.savePDF()
+
         }
 
         clear.setOnClickListener{
--- app/src/main/java/com/example/pdf/Library_Pdfiara.kt
+++ app/src/main/java/com/example/pdf/Library_Pdfiara.kt
@@ -1,102 +1,97 @@
 package com.example.pdf
 
 import android.os.Environment
-import android.widget.Toast
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
-import java.io.OutputStreamWriter
-import java.lang.StringBuilder
+
+
+
+
+
+
 
 class PdfGenerator {
+    var fileName = ""
+    private var object_counter = 1
+    private var page_counter = 1
+    private var position = 700
+    private var textBoxes = 0
 
 
 
 
 
-    companion object {
-        @JvmStatic
-        fun generatePdf(content_: String, filename: String) {
-            val filePath = File(
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString(),
-                filename
-            )
-
-            try {
-                val outputStream = FileOutputStream(filePath)
-                outputStream.write("%PDF-1.5\n".toByteArray())
-
-                outputStream.write("1 0 obj\n".toByteArray())
-                outputStream.write("<< /Type /Catalog /Pages 2 0 R >>\n".toByteArray())
-                outputStream.write("endobj\n".toByteArray())
-
-                outputStream.write("2 0 obj\n".toByteArray())
-                outputStream.write("<< /Type /Pages /Kids [3 0 R] /Count 1 >>\n".toByteArray())
-                outputStream.write("endobj\n".toByteArray())
-
-                outputStream.write("3 0 obj\n".toByteArray())
-                outputStream.write("<< /Type /Page /Parent 2 0 R /Resources << /Font << /F1 4 0 R >> >> /Contents 5 0 R >>\n".toByteArray())
-                outputStream.write("endobj\n".toByteArray())
-
-                outputStream.write("4 0 obj\n".toByteArray())
-                outputStream.write("<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\n".toByteArray())
-                outputStream.write("endobj\n".toByteArray())
 
 
 
 
 
-                outputStream.write("5 0 obj\n".toByteArray())
+    fun addText(text_: String) {  // dodanie tekstu do pliku
+
+
+        val filePath = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString(),
+            fileName
+        )
+
+
+
+        try {
+            val outputStream = FileOutputStream(filePath, true)
+
+            var text = text_
+            text = text.trim()
+            var text_temp: String
+            var index = 0
+
+            if (textBoxes == 0){  // zwinięcie do if żeby można wywoływać funkcję 'addText' wiele razy
+                outputStream.write("%PDF-1.7 \n".toByteArray())
+                outputStream.write("$object_counter 0 obj\n".toByteArray())
                 outputStream.write("<< /Length 44 >>\n".toByteArray())
                 outputStream.write("stream\n".toByteArray())
-
-                var content = content_
-                content = content.trim()
-                var content_temp: String
-                var position = 700  // wysokość na jakiej wypisywany jest tekst (liczone od dołu)
-                var index = 0
-
-                while (content.length >= 80){
-                    loop@ for (i in 80 downTo 0){  // szukamy ostatniej spacji
-                        if (content[i].toString().equals(" ")){
-                            index = i
-                            break@loop
-                        }
-                    }
-
-                    // wycinamy i wypisujemy tekst o długości 80-odległość do ostatniej spacji
-                    content_temp = content.substring(0, index)
-                    outputStream.write("BT /F1 12 Tf 70 $position Td ($content_temp) Tj ET \n".toByteArray())
-                    content = content.substring(index)
-                    position -= 30
-                }
-                outputStream.write("BT /F1 12 Tf 70 $position Td ($content) Tj ET \n".toByteArray())  // wypisujemy reszte stringa która jest krótsza od 80
-                outputStream.write("endstream\n".toByteArray())
-                outputStream.write("endobj\n".toByteArray())
-
-
-
-
-
-                outputStream.write("xref\n".toByteArray())
-
-                outputStream.write("0 6\n".toByteArray())
-                outputStream.write("0000000000 65535 f \n".toByteArray())
-                outputStream.write("0000000010 00000 n \n".toByteArray())
-                outputStream.write("0000000055 00000 n \n".toByteArray())
-                outputStream.write("0000000110 00000 n \n".toByteArray())
-                outputStream.write("0000000180 00000 n \n".toByteArray())
-                outputStream.write("0000000275 00000 n \n".toByteArray())
-
-                outputStream.write("trailer\n".toByteArray())
-                outputStream.write("<< /Size 6 /Root 1 0 R >>\n".toByteArray())
-                outputStream.write("startxref\n".toByteArray())
-                outputStream.write("357\n".toByteArray())
-                outputStream.write("%%EOF\n".toByteArray())
-                outputStream.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
             }
+            textBoxes++
+
+            while (text.length >= 80){
+                if (position <= 90){  // gdy tekst dojdzie wystarczająco nisko tworzymy nową stronę
+                    // zamykamy poprzednią strone
+                    outputStream.write("endstream\n".toByteArray())
+                    outputStream.write("endobj\n".toByteArray())
+
+                    // otwieramy nową strone
+                    outputStream.write("$object_counter 0 obj\n".toByteArray())
+                    outputStream.write("<< /Length 44 >>\n".toByteArray())
+                    outputStream.write("stream\n".toByteArray())
+                    object_counter++
+
+                    position = 700  // ustawiamy pozycję na górę strony
+                    page_counter++
+                    textBoxes++
+                }
+
+                loop@ for (i in 80 downTo 0){  // szukamy ostatniej spacji
+                    if (text[i].toString().equals(" ")){
+                        index = i
+                        break@loop
+                    }
+                }
+
+                // wycinamy i wypisujemy tekst o długości 80-odległość do ostatniej spacji
+                text_temp = text.substring(0, index)
+                outputStream.write("BT /F1 12 Tf 70 $position Td ($text_temp) Tj ET \n".toByteArray())
+                text = text.substring(index)
+                text = text.trim()
+                position -= 30
+            }
+            outputStream.write("BT /F1 12 Tf 70 $position Td ($text) Tj ET \n".toByteArray())  // wypisujemy reszte stringa która jest krótsza od 80
+            position -= 30
+
+
+            outputStream.close()
+
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 
@@ -104,4 +99,87 @@ class PdfGenerator {
 
 
 
+
+
+
+
+
+    fun saveText() {  // zakończenie edycji tekstu w pliku
+        val filePath = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString(),
+            fileName
+        )
+
+        try {
+            val outputStream = FileOutputStream(filePath, true)
+
+
+            // zamknięcie textu z funkcji 'addText'
+            outputStream.write("endstream\n".toByteArray())
+            outputStream.write("endobj\n".toByteArray())
+            object_counter++
+
+            outputStream.write("$object_counter 0 obj\n".toByteArray())
+            outputStream.write("<< /Type /Catalog /Pages ${object_counter+1} 0 R >>\n".toByteArray())
+            outputStream.write("endobj\n".toByteArray())
+            object_counter++
+
+            var temp_kids = ""
+            for (page_number in 1 .. page_counter){
+                temp_kids += "${object_counter+page_number} 0 R "
+            }
+            outputStream.write("$object_counter 0 obj\n".toByteArray())
+            outputStream.write("<< /Type /Pages /Kids [$temp_kids] /Count $page_counter >>\n".toByteArray())
+            outputStream.write("endobj\n".toByteArray())
+            object_counter++
+
+            for (page_number in 1 .. page_counter){
+                outputStream.write("$object_counter 0 obj \n".toByteArray())
+                outputStream.write("<< /Type /Page /Parent ${object_counter-page_number} 0 R /Contents $page_number 0 R >> \n".toByteArray())
+                outputStream.write("endobj \n".toByteArray())
+                object_counter++
+            }
+
+
+            outputStream.close()
+
+        }catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    fun savePDF() {
+        val filePath = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString(),
+            fileName
+        )
+
+        try {
+            val outputStream = FileOutputStream(filePath, true)
+
+            outputStream.write("trailer\n".toByteArray())
+            outputStream.write("<< /Size ${object_counter-1} /Root ${page_counter+1} 0 R >>\n".toByteArray())
+            outputStream.write("startxref\n".toByteArray())
+            outputStream.write("357\n".toByteArray())
+            outputStream.write("%%EOF\n".toByteArray())
+
+            outputStream.close()
+
+        }catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
 }
+
+
+
+
