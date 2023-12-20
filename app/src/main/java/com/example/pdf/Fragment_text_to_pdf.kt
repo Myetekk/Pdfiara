38e0e6b69b7798d486de0269aa0416833283225b
Myetekk <mirekmich19@gmail.com>
2023-12-20 13:30:02 +0100

    dodanie tekstu po współrzędnychi dodanie komentarzy do kodu

--- app/src/main/java/com/example/pdf/Fragment_text_to_pdf.kt
+++ app/src/main/java/com/example/pdf/Fragment_text_to_pdf.kt
@@ -38,16 +38,17 @@ class Fragment_text_to_pdf : Fragment() {
         save.setOnClickListener{
 
             textView_text_to_pdf = view.findViewById<TextView>(R.id.textView_text_to_pdf)
-            val content = textView_text_to_pdf.text.toString()
-//            val content = "W tym tygodniu zatwierdzono slawetny aneks nr. 4. Co to oznacza? To ze w tym momencie mamy klepniete uzbrojenie i wyposazenie dla WSZYSTKICH TRZECH fregat rakietowych budowanych w ramach programu Miecznik. Okej, to nie oznacza 100% wyposazenia i uzbrojenia ale cyferke stojaca bardzo blisko tych 100% ktora uzasadnia stwierdzenie ze dla wszystkich 3 przyszlych fregat mamy uzbrojenie i wyposazenie. Koniec obaw o tym ze Miecznik skonczy jak Slazak 2.0 z gotowymi platformami ale bez pelnego uzbrojenia i wyposazenia, koniec obaw o to ze zamawiajac reszte wyposazenia i uzbrojenia okaze sie ze albo jest ono znacznie drozsze albo 'mamy tylko nowy model'. Przed programem stoja kolejne kroki milowe i kolejne wyzwania, zas przed nami dziennikarzami obowiazek dalszego informowania i przekonywania, tak spoleczenstwa jak i nowej ekipy MON, o tym jak potrzebne MW RP i Polsce sa Mieczniki.   "
+//            val content = textView_text_to_pdf.text.toString()
+            val content = "W tym tygodniu zatwierdzono slawetny aneks nr. 4. Co to oznacza? To ze w tym momencie mamy klepniete uzbrojenie i wyposazenie dla WSZYSTKICH TRZECH fregat rakietowych budowanych w ramach programu Miecznik. Okej, to nie oznacza 100% wyposazenia i uzbrojenia ale cyferke stojaca bardzo blisko tych 100% ktora uzasadnia stwierdzenie ze dla wszystkich 3 przyszlych fregat mamy uzbrojenie i wyposazenie. Koniec obaw o tym ze Miecznik skonczy jak Slazak 2.0 z gotowymi platformami ale bez pelnego uzbrojenia i wyposazenia, koniec obaw o to ze zamawiajac reszte wyposazenia i uzbrojenia okaze sie ze albo jest ono znacznie drozsze albo 'mamy tylko nowy model'. Przed programem stoja kolejne kroki milowe i kolejne wyzwania, zas przed nami dziennikarzami obowiazek dalszego informowania i przekonywania, tak spoleczenstwa jak i nowej ekipy MON, o tym jak potrzebne MW RP i Polsce sa Mieczniki.   "
 
 
             val newFile1 = PdfGenerator()
             newFile1.fileName = "PDFiara_text.pdf"
-            newFile1.addText(content)
-//            newFile1.addText("Taka sobie druga linia ")
-//            newFile1.addText("Jakas trzecia linia ")
-//            newFile1.addText("A tu czwarta ")
+            newFile1.addText(content+content)
+            newFile1.addText("Taka sobie druga linia ")
+            newFile1.addText("Jakas trzecia linia ")
+            newFile1.addText("A tu czwarta ")
+            newFile1.addText("tekst podany ze współrzednymi", 300, 730)
             newFile1.savePDF()
         }
 
--- app/src/main/java/com/example/pdf/Library_Pdfiara.kt
+++ app/src/main/java/com/example/pdf/Library_Pdfiara.kt
@@ -13,8 +13,10 @@ import java.io.IOException
 
 private class TextBox {
     var text: String = ""
-    //var x: Int = 0
-    //var y: Int = 0
+    var x: Int = 70
+    var y: Int = 700
+
+    var withCordinates: Boolean = false    // określa czy współrzędne mają się określić automatycznie czy zostały wprowadzone ręcznie
 }
 
 
@@ -41,9 +43,10 @@ class PdfGenerator {
 
     private var object_counter = 1
     private var page_counter = 1
-    private var position = 700
-    private var textBoxes = 0
-    private var texts : MutableList<TextBox> = ArrayList()
+    private var position_x = 70    // pozycja x tekstu na stronie
+    private var position_y = 700    // pozycja y tekstu na stronie
+    private var textBoxes = 0    // licznik textBoxów, potrzebny tylko aby zacząć plik
+    private var texts: MutableList<TextBox> = ArrayList()    // lista textBoxów zawierająca wszystkie textBoxy XD
 
 
 
@@ -54,15 +57,23 @@ class PdfGenerator {
 
 
 
-    fun addText(text: String) {
-        val newText = TextBox()
-        newText.text = text
-        texts.add(newText)
+    fun addText(text: String) {    // funkcja dodawania tekstu bez współrzędnych
+        val newText = TextBox()    // tworzymy nowy obiekt klasy textBox
+        newText.text = text    // ustawiamy jego text
+        newText.withCordinates = false    // ustalamy że współrzędne mają się określić automatycznie
+
+        texts.add(newText)    // dodajemy nowego textBoxa do listy textBoxów
     }
 
-//    fun addText(text: String, x: Int, y: Int) {  // przyszła funkcja dodawania tekstu na podanych współrzędnych
-//
-//    }
+    fun addText(text: String, x: Int, y: Int) {    // funkcja dodawania tekstu z współrzędnymi
+        val newText = TextBox()    // tworzymy nowy obiekt klasy textBox
+        newText.text = text    // ustawiamy jego text
+        newText.x = x    // ustawiamy jego współrzędną X
+        newText.y = y    // ustawiamy jego współrzędną Y
+        newText.withCordinates = true    // ustalamy że współrzędne zostały określone ręcznie
+
+        texts.add(newText)    // dodajemy nowego textBoxa do listy textBoxów
+    }
 
 
 
@@ -75,12 +86,10 @@ class PdfGenerator {
 
     fun savePDF() {
         // zapisywanie wszystkich tekstów
-        // wpisanie wszystkich textboxów do pliku
-        for (text in texts){
-            addTextToPDF(text.text)
+        for (text in texts){    // wpisanie wszystkich textboxów do pliku
+            addTextToPDF(text)
         }
-        saveText()
-
+        saveText()    // dodajemy stopke textów do pliku
 
         // zapisywanie wszystkich obrazów
 
@@ -89,7 +98,7 @@ class PdfGenerator {
 
 
         // zapisanie stopki
-        addTrailer()
+        addTrailer()    // dodajemy stopke całego pliku PDF
     }
 
 
@@ -101,7 +110,85 @@ class PdfGenerator {
 
 
 
-    private fun saveText() {  // zakończenie edycji tekstu w pliku
+    private fun addTextToPDF(textBox_: TextBox) {    // dodanie tekstu do pliku
+        val filePath = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString(),
+            fileName
+        )
+
+
+        try {
+            val outputStream = FileOutputStream(filePath, true)
+
+            var text = textBox_.text    // przypisujemy text danego textBoxa do zmiennej lokalnej
+            position_x = textBox_.x    // przypisujemy współrzędną X danego textBoxa do zmiennej lokalnej
+            if (textBox_.withCordinates == true) position_y = textBox_.y    // jeśli użytkownik określił współrzędne ręcznie to przypisujemy współrzędną X danego textBoxa do zmiennej lokalnej
+            text = text.trim()
+
+            var text_temp: String
+            var index_space = 0
+
+            if (textBoxes == 0){    // zwinięcie do if żeby można wywoływać funkcję 'addText' wiele razy
+                outputStream.write("%PDF-1.7 \n".toByteArray())
+                outputStream.write("$object_counter 0 obj\n".toByteArray())
+                outputStream.write("<< /Length 44 >>\n".toByteArray())
+                outputStream.write("stream\n".toByteArray())
+            }
+            textBoxes++
+
+            while (text.length >= 80){    // gdy tekst jest dłuższy niż 80 znaków zawijamy go do nowej lini
+                if (position_y <= 90){    // gdy tekst dojdzie wystarczająco nisko tworzymy nową stronę
+                    // zamykamy poprzednią strone
+                    outputStream.write("endstream\n".toByteArray())
+                    outputStream.write("endobj\n".toByteArray())
+                    object_counter++
+
+                    // otwieramy nową strone
+                    outputStream.write("$object_counter 0 obj\n".toByteArray())
+                    outputStream.write("<< /Length 44 >>\n".toByteArray())
+                    outputStream.write("stream\n".toByteArray())
+
+                    position_y = 700    // ustawiamy pozycję na górę strony
+                    page_counter++
+                    textBoxes++
+                }
+
+                loop@ for (i in 80 downTo 0){    // szukamy ostatniej spacji żeby nie ucinać słowa w połowie
+                    if (text[i].toString().equals(" ")){
+                        index_space = i
+                        break@loop
+                    }
+                }
+
+                // wycinamy i wypisujemy tekst o długości 80-odległość do ostatniej spacji
+                text_temp = text.substring(0, index_space)    // wycinamy stringa od początku do indexu ostatniej spacji przed przekroczeniem 80 znaków w linijce
+                outputStream.write("BT /F1 12 Tf $position_x $position_y Td ($text_temp) Tj ET \n".toByteArray())    // i wypisujemy go
+                text = text.substring(index_space)    // wycinamy reszte stringa i nadpisujemy głównego stringa
+                text = text.trim()
+                position_y -= 30    // ustawiamy współrzędną Y tak aby dać efekt zejścia liniję niżej
+            }
+
+            outputStream.write("BT /F1 12 Tf $position_x $position_y Td ($text) Tj ET \n".toByteArray())    // wypisujemy reszte stringa która jest krótsza od 80
+            position_y -= 30    // ustawiamy współrzędną Y tak aby dać efekt zejścia liniję niżej
+
+
+            outputStream.close()
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    private fun saveText() {    // zakończenie edycji tekstu w pliku
         val filePath = File(
             Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString(),
             fileName
@@ -117,11 +204,13 @@ class PdfGenerator {
             outputStream.write("endobj\n".toByteArray())
             object_counter++
 
+            // deklarujemy gdzie znajdują się deklaracje stron
             outputStream.write("$object_counter 0 obj\n".toByteArray())
             outputStream.write("<< /Type /Catalog /Pages ${object_counter+1} 0 R >>\n".toByteArray())
             outputStream.write("endobj\n".toByteArray())
             object_counter++
 
+            // deklarujemy gdzie znajdują się deklaracje zawartości stron
             var temp_kids = ""
             for (page_number in 1 .. page_counter){
                 temp_kids += "${object_counter+page_number} 0 R "
@@ -131,6 +220,7 @@ class PdfGenerator {
             outputStream.write("endobj\n".toByteArray())
             object_counter++
 
+            // deklaracje zawartości stron
             for (page_number in 1 .. page_counter){
                 outputStream.write("$object_counter 0 obj \n".toByteArray())
                 outputStream.write("<< /Type /Page /Parent ${object_counter-page_number} 0 R /Contents $page_number 0 R >> \n".toByteArray())
@@ -155,82 +245,7 @@ class PdfGenerator {
 
 
 
-    private fun addTextToPDF(text_: String) {  // dodanie tekstu do pliku
-        val filePath = File(
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString(),
-            fileName
-        )
-
-
-        try {
-            val outputStream = FileOutputStream(filePath, true)
-
-            var text = text_
-            text = text.trim()
-            var text_temp: String
-            var index_space = 0
-
-            if (textBoxes == 0){  // zwinięcie do if żeby można wywoływać funkcję 'addText' wiele razy
-                outputStream.write("%PDF-1.7 \n".toByteArray())
-                outputStream.write("$object_counter 0 obj\n".toByteArray())
-                outputStream.write("<< /Length 44 >>\n".toByteArray())
-                outputStream.write("stream\n".toByteArray())
-            }
-            textBoxes++
-
-            while (text.length >= 80){
-                if (position <= 90){  // gdy tekst dojdzie wystarczająco nisko tworzymy nową stronę
-                    // zamykamy poprzednią strone
-                    outputStream.write("endstream\n".toByteArray())
-                    outputStream.write("endobj\n".toByteArray())
-                    object_counter++
-
-                    // otwieramy nową strone
-                    outputStream.write("$object_counter 0 obj\n".toByteArray())
-                    outputStream.write("<< /Length 44 >>\n".toByteArray())
-                    outputStream.write("stream\n".toByteArray())
-
-                    position = 700  // ustawiamy pozycję na górę strony
-                    page_counter++
-                    textBoxes++
-                }
-
-                loop@ for (i in 80 downTo 0){  // szukamy ostatniej spacji
-                    if (text[i].toString().equals(" ")){
-                        index_space = i
-                        break@loop
-                    }
-                }
-
-                // wycinamy i wypisujemy tekst o długości 80-odległość do ostatniej spacji
-                text_temp = text.substring(0, index_space)
-                outputStream.write("BT /F1 12 Tf 70 $position Td ($text_temp) Tj ET \n".toByteArray())
-                text = text.substring(index_space)
-                text = text.trim()
-                position -= 30
-            }
-
-            outputStream.write("BT /F1 12 Tf 70 $position Td ($text) Tj ET \n".toByteArray())  // wypisujemy reszte stringa która jest krótsza od 80
-            position -= 30
-
-
-            outputStream.close()
-
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-    private fun addTrailer() {
+    private fun addTrailer() {    // zakończenie edycji pliku
         val filePath = File(
             Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString(),
             fileName
@@ -239,6 +254,7 @@ class PdfGenerator {
         try {
             val outputStream = FileOutputStream(filePath, true)
 
+            // deklarujemy ilość obiektów, lokaizacje głównego obiektu i zamykamy plik
             outputStream.write("trailer\n".toByteArray())
             outputStream.write("<< /Size ${object_counter-1} /Root ${page_counter+1} 0 R >>\n".toByteArray())
             outputStream.write("startxref\n".toByteArray())
